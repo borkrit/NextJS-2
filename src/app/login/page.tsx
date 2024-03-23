c92e0456d683dd68a7a72b7e174bf302cd46f89c
Vladyslav Pohorilyi <pogoriliy15@gmail.com>
2024-03-23 22:11:52 +0100

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,18 +7,18 @@ export default function Login() {
   const router = useRouter()
   const ref = useRef()
 
-  const handelSubmit = (event: { preventDefault: () => void; })=>{
-    event.preventDefault();
-    authUser(ref.current?.value)
-    router.push(`/login-progress`);
+  // const handelSubmit = (event: { preventDefault: () => void; })=>{
+  //   event.preventDefault();
+  //   authUser(ref.current?.value)
+  //   router.push(`/login-progress`);
 
-  }
+  // }
   
   return (
     <>
       <main>
         
-          <form onSubmit={handelSubmit} className="flex m-auto gap-2  flex-col max-w-[300px]">
+          <form  className="flex m-auto gap-2  flex-col max-w-[300px]">
 
             <input ref={ref} type="text" className=" border-2  rounded border-y-slate-400 px-2 py-3"  placeholder="login" />
 
