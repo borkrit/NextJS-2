fdd6696e13243636e7607b700c8abb02bcbc9dbb
Vladyslav Pohorilyi <pogoriliy15@gmail.com>
2024-03-24 18:00:45 +0100

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,11 @@
 "use client"
-import { useRouter } from "next/navigation";
-import { useRef } from "react";
-import {authUser} from '@/app/action'
+// import { useRouter } from "next/navigation";
+// import { useRef } from "react";
+// import {authUser} from '@/app/action'
 
 export default function Login() {
-  const router = useRouter()
-  const ref = useRef()
+  // const router = useRouter()
+  // const ref = useRef()
 
   // const handelSubmit = (event: { preventDefault: () => void; })=>{
   //   event.preventDefault();
@@ -16,16 +16,7 @@ export default function Login() {
   
   return (
     <>
-      <main>
-        
-          <form  className="flex m-auto gap-2  flex-col max-w-[300px]">
-
-            <input ref={ref} type="text" className=" border-2  rounded border-y-slate-400 px-2 py-3"  placeholder="login" />
-
-            <button className=" bg-red-200 " type="submit" > login</button>
-          </form>
-
-      </main>
+     <main><h2>Login page</h2></main>
     </>
   );
 }
